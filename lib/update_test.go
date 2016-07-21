b7588a79735eac3e6a5036e6c893a0e4a2490fc2
dengwu12 <ting.zhangting@alibaba-inc.com>
2016-07-21 16:05:56 +0800

    add case for update

---
+++ lib/update_test.go
@@ -0,0 +1,73 @@
+package lib 
+
+import (
+    "os"
+
+    . "gopkg.in/check.v1"
+)
+
+func (s *OssutilCommandSuite) rawUpdate(force bool, language string) (bool, error) {
+    command := "update" 
+    var args []string
+    options := OptionMapType{
+        OptionForce: &force,
+        OptionLanguage: &language, 
+    }
+    showElapse, err := cm.RunCommand(command, args, options)
+    return showElapse, err
+}
+
+func (s *OssutilCommandSuite) TestUpdate(c *C) {
+    showElapse, err := s.rawUpdate(false, "中文")
+    c.Assert(err, IsNil)
+    c.Assert(showElapse, Equals, false)
+
+    showElapse, err = s.rawUpdate(false, "English")
+    c.Assert(err, IsNil)
+    c.Assert(showElapse, Equals, false)
+
+    showElapse, err = s.rawUpdate(true, "中文")
+
+    showElapse, err = s.rawUpdate(true, "English")
+
+    err = updateCommand.updateVersion("中文")
+}
+
+func (s *OssutilCommandSuite) TestDownloadLastestBinary(c *C) {
+    tempBinaryFile := ".ossutil_test_update.temp"  
+    err := updateCommand.getLastestBinary(tempBinaryFile) 
+    c.Assert(err, IsNil)
+
+    _ = os.Remove(tempBinaryFile)
+}
+
+func (s *OssutilCommandSuite) TestAnonymousGetToFileError(c *C) {
+    bucket := bucketNamePrefix + "update"
+    object := "object"
+    err := updateCommand.anonymousGetToFileRetry(bucket, object, object)
+    c.Assert(err, NotNil)
+
+    s.putBucket(bucket, c)
+    s.putObject(bucket, object, uploadFileName, c)
+    fileName := "*"
+    err = updateCommand.anonymousGetToFileRetry(bucket, object, fileName)
+    c.Assert(err, NotNil)
+}
+
+func (s *OssutilCommandSuite) TestRevertRename(c *C) {
+    filePath := ".ossutil_tempf"
+    renameFilePath := ".ossutil_tempr"
+
+    s.createFile(filePath, filePath + "i", c)
+    s.createFile(renameFilePath, renameFilePath + "i", c)
+
+    updateCommand.revertRename(filePath, renameFilePath)
+    _, err := os.Stat(renameFilePath) 
+    c.Assert(err, NotNil)
+
+    str := s.readFile(filePath, c) 
+    c.Assert(str, Equals, renameFilePath + "i")
+
+    _ = os.Remove(filePath)
+    _ = os.Remove(renameFilePath)
+}
