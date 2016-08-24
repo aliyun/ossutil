c46283123335c13975bc8711110180476b8cd767
dengwu12 <ting.zhangting@alibaba-inc.com>
2016-08-24 16:45:40 +0800

    modify version

--- lib/const.go
+++ lib/const.go
@@ -51,7 +51,7 @@ const (
 const (
 	Package             string = "ossutil"
 	ChannelBuf          int    = 1000
-	Version             string = "1.0.0.Beta"
+	Version             string = "1.0.0"
 	DefaultEndpoint     string = "oss-cn-hangzhou.aliyuncs.com"
 	DefaultLanguage            = "中文"
     EnglishLanguage            = "English"
