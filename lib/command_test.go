e77ba0889762c12e7d17075f0d44f1003622a558
dengwu12 <ting.zhangting@alibaba-inc.com>
2016-11-16 16:07:43 +0800

    modify case

--- lib/command_test.go
+++ lib/command_test.go
@@ -49,7 +49,7 @@ var (
     cm                  = CommandManager{}
     out                 = os.Stdout
     errout              = os.Stderr
-    sleepTime           = 7*time.Second
+    sleepTime           = 8*time.Second
 )
 
 // Run once when the suite starts running
