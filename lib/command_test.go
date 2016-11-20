ced0791598308491cbd493c4346758ab54617395
dengwu12 <ting.zhangting@alibaba-inc.com>
2016-11-20 08:59:59 +0800

    modify case

--- lib/command_test.go
+++ lib/command_test.go
@@ -110,20 +110,20 @@ func (s *OssutilCommandSuite) SetUpBucketEnv(c *C) {
     for _, bucket := range []string{bucketNameExist, bucketNameDest, bucketNameCP, bucketNameBCP, bucketNameSetMeta, bucketNameSetMeta1, bucketNameSetACL, bucketNameSetACL1, bucketNameMB, bucketNameList} { 
         s.putBucket(bucket, c)
     }
-    time.Sleep(3*sleepTime)
+    time.Sleep(sleepTime)
     for _, bucket := range []string{bucketNameExist, bucketNameDest, bucketNameCP, bucketNameBCP, bucketNameSetMeta, bucketNameSetMeta1, bucketNameSetACL, bucketNameSetACL1, bucketNameMB, bucketNameList} { 
         s.removeObjects(bucket, "", true, true, c)
         time.Sleep(sleepTime)
     }
-    time.Sleep(sleepTime)
+    for _, bucket := range []string{bucketNameExist, bucketNameDest, bucketNameCP, bucketNameBCP, bucketNameSetMeta, bucketNameSetMeta1, bucketNameSetACL, bucketNameSetACL1, bucketNameMB, bucketNameList} { 
+        s.removeObjects(bucket, "", true, true, c)
+        time.Sleep(sleepTime)
+    }
 }
 
 // Run before each test or benchmark starts running
 func (s *OssutilCommandSuite) TearDownSuite(c *C) {
     testLogger.Println("test command completed")
-    for _, bucket := range []string{bucketNameExist, bucketNameDest, bucketNameCP, bucketNameBCP, bucketNameSetMeta, bucketNameSetMeta1, bucketNameSetACL, bucketNameSetACL1, bucketNameMB, bucketNameList} { 
-        s.removeBucket(bucket, true, c)
-    }
     _ = os.Remove(configFile)
     _ = os.Remove(resultPath)
     _ = os.Remove(uploadFileName)
