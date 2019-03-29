cf4ec798972ce5e3677c3800ea1c8c739f16ccdb
taowei.wtw <taowei.wtw@alibaba-inc.com>
2019-03-29 16:30:24 +0800

    add test for referer command

--- lib/bucket_referer.go
+++ lib/bucket_referer.go
@@ -7,9 +7,97 @@ import (
 	"strings"
 )
 
-var specChineseBucketRefer = SpecText{}
+var specChineseBucketReferer = SpecText{
+	synopsisText: "设置、查询或者删除bucket的referer配置",
 
-var specEnglishBucketRefer = SpecText{}
+	paramText: "bucket_url referer [options]",
+
+	syntaxText: ` 
+	ossutil referer --method put oss://bucket referer [options]
+    ossutil referer --method get oss://bucket [local_file]
+    ossuitl referer --method delete oss://bucket
+`,
+	detailHelpText: ` 
+    referer命令通过设置method选项值为put、get、delete,可以设置、查询或者删除bucket的referer配置
+
+用法:
+    该命令有三种用法:
+	
+    1) ossutil referer --method put oss://bucket referer [options]
+       这个命令将bucket的referer设置成后面的referer值
+       referer参数可以连续输入多个
+        
+    2) ossutil referer --method get oss://bucket  [local_xml_file]
+        这个命令查询bucket的referer配置
+        如果输入参数local_xml_file，referer配置将输出到该文件，否则输出到屏幕上
+	
+    3)  ossutil referer --method delete oss://bucket
+        这个命令删除bucket的referer配置
+`,
+	sampleText: ` 
+    1) 设置bucket的referer配置
+       ossutil referer --method put oss://bucket www.test1.com www.test2.com
+	
+    2) 设置bucket的referer配置，且不允许referer为空
+       ossutil referer --method put oss://bucket www.test1.com www.test2.com  --disable-empty-referer
+
+    3) 查询bucket的referer配置，结果输出到标准输出
+       ossutil referer --method get oss://bucket
+	
+    4) 查询bucket的referer配置，结果输出到本地文件
+       ossutil referer --method get oss://bucket local_xml_file
+	
+    5) 删除bucket的referer配置
+       ossutil referer --method delete oss://bucket
+`,
+}
+
+var specEnglishBucketReferer = SpecText{
+	synopsisText: "Set、get or delete bucket referer configuration",
+
+	paramText: "bucket_url referer [options]",
+
+	syntaxText: ` 
+	ossutil referer --method put oss://bucket referer [options]
+    ossutil referer --method get oss://bucket [local_file]
+    ossuitl referer --method delete oss://bucket
+`,
+	detailHelpText: ` 
+    referer command can set、get and delete the referer configuration of the oss bucket by
+    set method option value to put, get,delete
+
+Usage:
+    There are three usages for this command:
+	
+    1) ossutil referer --method put oss://bucket referer [options]
+       This command sets the referer of the bucket to the following referer value.
+       You can input many referer parameter.
+        
+    2) ossutil referer --method get oss://bucket  [local_xml_file]
+       The command gets the referer configuration of bucket
+       If you input parameter local_xml_file,the configuration will be output to local_xml_file
+       If you don't input parameter local_xml_file,the configuration will be output to stdout
+	
+    3)  ossutil referer --method delete oss://bucket
+       The command deletes the referer configuration of bucket
+`,
+	sampleText: ` 
+    1) put bucket referer
+       ossutil referer --method put oss://bucket www.test1.com www.test2.com
+	
+    2) put bucket referer, empty referer is forbidden  
+       ossutil referer --method put oss://bucket www.test1.com www.test2.com --disable-empty-referer
+
+    3) get referer configuration to stdout
+       ossutil referer --method get oss://bucket
+	
+    4) get referer configuration to local file
+       ossutil referer --method get oss://bucket local_xml_file
+	
+    5) delete referer configuration
+       ossutil referer --method delete oss://bucket
+`,
+}
 
 type bucketReferOptionType struct {
 	bucketName        string
@@ -25,10 +113,10 @@ var bucketRefererCommand = BucketRefererCommand{
 	command: Command{
 		name:        "referer",
 		nameAlias:   []string{"referer"},
-		minArgc:     2,
+		minArgc:     1,
 		maxArgc:     MaxInt,
-		specChinese: specChineseBucketRefer,
-		specEnglish: specEnglishBucketRefer,
+		specChinese: specChineseBucketReferer,
+		specEnglish: specEnglishBucketReferer,
 		group:       GroupTypeNormalCommand,
 		validOptionNames: []string{
 			OptionConfigFile,
@@ -38,6 +126,7 @@ var bucketRefererCommand = BucketRefererCommand{
 			OptionSTSToken,
 			OptionLogLevel,
 			OptionDisableEmptyReferer,
+			OptionMethod,
 		},
 	},
 }
@@ -58,12 +147,17 @@ func (brc *BucketRefererCommand) Init(args []string, options OptionMapType) erro
 
 // RunCommand simulate inheritance, and polymorphism
 func (brc *BucketRefererCommand) RunCommand() error {
-	strMethod := brc.command.args[0]
-	if strMethod != "put" && strMethod != "get" && strMethod != "delete" {
-		return fmt.Errorf("%s is not in the optional value:put|get|delete", strMethod)
+	strMethod, _ := GetString(OptionMethod, brc.command.options)
+	if strMethod == "" {
+		return fmt.Errorf("--method value is empty")
 	}
 
-	srcBucketUrL, err := brc.CheckBucketUrl(brc.command.args[1], "")
+	strMethod = strings.ToLower(strMethod)
+	if strMethod != "put" && strMethod != "get" && strMethod != "delete" {
+		return fmt.Errorf("--method value is not in the optional value:put|get|delete")
+	}
+
+	srcBucketUrL, err := GetCloudUrl(brc.command.args[0], "")
 	if err != nil {
 		return err
 	}
@@ -81,29 +175,12 @@ func (brc *BucketRefererCommand) RunCommand() error {
 	return err
 }
 
-func (brc *BucketRefererCommand) CheckBucketUrl(strlUrl, encodingType string) (*CloudURL, error) {
-	bucketUrL, err := StorageURLFromString(strlUrl, encodingType)
-	if err != nil {
-		return nil, err
-	}
-
-	if !bucketUrL.IsCloudURL() {
-		return nil, fmt.Errorf("parameter is not a cloud url,url is %s", bucketUrL.ToString())
-	}
-
-	cloudUrl := bucketUrL.(CloudURL)
-	if cloudUrl.bucket == "" {
-		return nil, fmt.Errorf("bucket name is empty,url is %s", bucketUrL.ToString())
-	}
-	return &cloudUrl, nil
-}
-
 func (brc *BucketRefererCommand) PutBucketRefer() error {
-	if len(brc.command.args) < 3 {
-		return fmt.Errorf("put bucket referer need at least 3 parameters,the refer is empty")
+	if len(brc.command.args) < 2 {
+		return fmt.Errorf("put bucket referer need at least 2 parameters,the refer is empty")
 	}
 
-	referers := brc.command.args[2:len(brc.command.args)]
+	referers := brc.command.args[1:len(brc.command.args)]
 
 	// put bucket refer
 	client, err := brc.command.ossClient(brc.brOption.bucketName)
@@ -140,8 +217,8 @@ func (brc *BucketRefererCommand) GetBucketRefer() error {
 	}
 
 	var outFile *os.File
-	if len(brc.command.args) >= 3 {
-		fileName := brc.command.args[2]
+	if len(brc.command.args) >= 2 {
+		fileName := brc.command.args[1]
 		_, err = os.Stat(fileName)
 		if err == nil {
 			bConitnue := brc.confirm(fileName)
