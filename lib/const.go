4f98c17b7535f25f4bb4b3ae78829d8f663224ed
taowei.wtw <taowei.wtw@alibaba-inc.com>
2019-10-31 17:48:05 +0800

    optimize the description of config command

--- lib/const.go
+++ lib/const.go
@@ -120,7 +120,7 @@ const (
 const (
 	Package                 string = "ossutil"
 	ChannelBuf              int    = 1000
-	Version                 string = "v1.6.8"
+	Version                 string = "v1.6.9"
 	DefaultEndpoint         string = "oss.aliyuncs.com"
 	ChineseLanguage                = "CH"
 	EnglishLanguage                = "EN"
