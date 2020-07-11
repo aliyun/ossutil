04dff6f4143e2c67c6319dae0d50d4ed251132d1
taowei.wtw <taowei.wtw@alibaba-inc.com>
2020-07-11 14:26:17 +0800

    rebuild with oss go sdk v2.1.3,lifecycle support ColdArchive

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 - The current version does not support bucket management. The feature will be available in future versions.
 
 ## Version
-- Current version: v1.6.16
+- Current version: v1.6.17
 
 ## Run environment
 - Linux
--- lib/const.go
+++ lib/const.go
@@ -129,7 +129,7 @@ const (
 const (
 	Package                 string = "ossutil"
 	ChannelBuf              int    = 1000
-	Version                 string = "v1.6.16"
+	Version                 string = "v1.6.17"
 	DefaultEndpoint         string = "oss.aliyuncs.com"
 	ChineseLanguage                = "CH"
 	EnglishLanguage                = "EN"
