ca73ce75f803a2fc3ba2bcbb8603ad5e4ef9f811
taowei.wtw <taowei.wtw@alibaba-inc.com>
2020-12-08 16:03:26 +0800

    add worm command

--- lib/const.go
+++ lib/const.go
@@ -130,7 +130,7 @@ const (
 const (
 	Package                 string = "ossutil"
 	ChannelBuf              int    = 1000
-	Version                 string = "v1.7.0"
+	Version                 string = "v1.7.1"
 	DefaultEndpoint         string = "oss.aliyuncs.com"
 	ChineseLanguage                = "CH"
 	EnglishLanguage                = "EN"
