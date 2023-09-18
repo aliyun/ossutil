87284dc8611faba2f30a67d6398abd29c09f96c4
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2023-09-18 13:20:51 +0800

    update version to v1.7.17

--- lib/const.go
+++ lib/const.go
@@ -156,7 +156,7 @@ const (
 const (
 	Package                 string = "ossutil"
 	ChannelBuf              int    = 1000
-	Version                 string = "v1.7.16"
+	Version                 string = "v1.7.17"
 	DefaultEndpoint         string = "oss.aliyuncs.com"
 	ChineseLanguage                = "CH"
 	EnglishLanguage                = "EN"
